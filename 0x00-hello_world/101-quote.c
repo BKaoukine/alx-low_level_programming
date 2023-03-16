18f4d26414f3700bd3bf2a9bc5842a7aa5c0defb
BKaoukine <b.koukin@gmail.com>
2023-03-16 11:55:49 -0700

    quote v4

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+  * main - starting Point
+  *
+  * Discription: this function outputs to STE
+  *
+  * Return: 1 for Error
+  */
+int main(void)
+	{
+
+		fprintf(stderr, "and that piece of art is useful\"" +
+		"- Dora Korpar, 2015-10-19\n");
+		return (0);
+	}
