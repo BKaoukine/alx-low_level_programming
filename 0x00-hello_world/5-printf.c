d9deb88eae32097c39c27f4961e012fb0862728c
BKaoukine <b.koukin@gmail.com>
2023-03-16 06:38:14 -0700

    printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'This function outputs the string to STO'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		/*
+		   * printf outputs the given string
+		*/
+		printf("with proper grammar, but the outcome is a piece of art,");
+		/*
+		   * the return value for success
+		*/
+		return (0);
+	}
