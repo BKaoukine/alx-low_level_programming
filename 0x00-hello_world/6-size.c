f6327cd836b8c478ea53a6b1ecaf23d8611440c9
BKaoukine <b.koukin@gmail.com>
2023-03-16 10:05:38 -0700

    size v15

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,6 @@ int main(void)
 		fprintf(stdout, "Size of a long int: %i byte(s)\n", sizeOfLongInt);
 		fprintf(stdout, "Size of a long long int: %i byte(s)\n", sizeOfLongLongInt);
 		fprintf(stdout, "Size of a float: %i byte(s)\n", sizeOfFloat);
-		fprintf(stderr, "%s", "[Anything]\n");
 
 		return (0);
 	}
