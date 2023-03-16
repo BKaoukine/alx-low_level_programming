86819bcab8a21c3ad8d558616b9fdbf3caffe824
BKaoukine <b.koukin@gmail.com>
2023-03-16 05:48:26 -0700

    puts Function v2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+	/*
+	   *This main function to print the string to STO
+	*/
 int main(void)
 	{
 		/*
 		*intilizing the string
 		 */
-		char string1[] = "Programming is like building a multilingual puzzle";
+		char string1[] = "\"Programming is like building a multilingual puzzle";
 		/*
 		*printing the string1
 		 */
