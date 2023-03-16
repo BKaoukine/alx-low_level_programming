d6b8317b373c14358c39ea0dc9605b88fd8175f5
BKaoukine <b.koukin@gmail.com>
2023-03-16 12:57:25 -0700

    quote v8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
   */
 int main(void)
 	{
-		char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		fprintf(stderr, quote);
+		char quote1[] = "and that piece of art is useful\"";
+		char quote2[] = "- Dora Korpar, 2015-10-19\n";
+
+		fprintf(stderr, % s, % s, quote1, quote2);
 		return (1);
 	}
