203c2b776ec060695d7b6757a258d3067dd8a2df
BKaoukine <b.koukin@gmail.com>
2023-03-16 05:44:34 -0700

    puts Function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+	{
+		/*
+		*intilizing the string
+		 */
+		char string1[] = "Programming is like building a multilingual puzzle";
+		/*
+		*printing the string1
+		 */
+
+		puts(string1);
+		return (0);
+	}
+
