01f2260954d48098cf6f0d8696be41d30350709b
BKaoukine <b.koukin@gmail.com>
2023-03-17 16:04:49 -0700

    advancedconbi

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+/* Online C Compiler and Editor */
+#include <stdio.h>
+/**
+  * main - Strating Point
+  *
+  * Discriptions: this program prints combination of two digits
+  *
+  *Return: 0 for Successe
+  */
+int main(void)
+{
+	int i, b;
+
+	for (i = 0 ; i <= 9 ; i++)
+				{
+	for (b = i ; b <= 9; b++)
+				{
+		if (i != 10 && b != 10)
+				{
+			if (b != i)
+				{
+				putchar('0' + i);
+				putchar('0' + b);
+				putchar(',');
+				putchar(' ');
+				}
+				}
+				}
+				}
+					return (0);
+}
