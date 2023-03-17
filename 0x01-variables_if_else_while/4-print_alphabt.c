cade03ec14c434ab025efacf5d2376b7d47c3d7b
BKaoukine <b.koukin@gmail.com>
2023-03-17 10:37:14 -0700

    alphaexp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Other headers goes here */
+
+/**
+  * main - Starting point
+  *
+  * Discription:  this programm prints the alphabets in lowercase
+  *
+  * Return: 0 for Successe
+  */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+		/**
+		 *char alphabets;
+		 *for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		 *{
+		 *putchar(alphabets);
+		 *}
+		 */
+	return (0);
+}
