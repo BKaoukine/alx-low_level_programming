4e266b32efa4c68b29293013bb9927f769f4e0dd
BKaoukine <b.koukin@gmail.com>
2023-03-17 10:58:36 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Other headers goes here */
+
+/**
+  * main - Starting point
+  *
+  * Discription:  this programm prints numbers from base 10
+  *
+  * Return: 0 for Successe
+  */
+int main(void)
+{
+	int numBase = 0;
+	char newLine = "\n";
+
+	while (numBase <= 9)
+	{
+		printf("%i");
+		numBase++;
+	}
+		/**
+		 *char alphabets;
+		 *for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		 *{
+		 *putchar(alphabets);
+		 *}
+		 */
+	putchar(newLine);
+	return (0);
+}
