a3cb11d5208922e887f01e0ef364891e1f412f96
BKaoukine <b.koukin@gmail.com>
2023-03-17 11:59:40 -0700

    numbz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Other headers goes here */
+
+/**
+  * main - Starting point
+  *
+  * Discription:  this programm prints numbers from base 10
+  *
+  * Return: 0 for Successe
+  */
+int main(void)
+{
+	int i = 0;
+	char newLine = '\n';
+
+	while (i <= 9)
+	{
+		printf('0', i);
+		i++;
+	}
+		/**
+		 *char alphabets;
+		 *for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		 *{
+		 *putchar(alphabets);
+		 *}
+		 */
+	putchar(newLine);
+	return (0);
+}
