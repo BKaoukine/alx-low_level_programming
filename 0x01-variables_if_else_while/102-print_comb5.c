d7947dac4c81f7e7b6e75c8614afaf82f86ff00c
BKaoukine <b.koukin@gmail.com>
2023-03-19 05:59:26 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,33 @@
-/* Online C Compiler and Editor */
-#include <stdio.h>
-/**
-  * main - Strating Point
-  *
-  * Discriptions: this program prints combination of three digits
-  *
-  *Return: 0 for Successe
-  */
 #include <stdio.h>
 
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i, j, k, l;
+	int i, j;
 
-    for (i = 0; i <= 99; i++) {
-        for (j = i; j <= 99; j++) {
-            k = i % 10;
-            l = j % 10;
-            i /= 10;
-            j /= 10;
-            if (i != 10 && j != 10 && i != l && j != k) {
-                putchar('0' + i);
-                putchar('0' + k);
-                putchar(' ');
-                putchar('0' + j);
-                putchar('0' + l);
-                if (k != 8 || l != 8) {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-            i *= 10;
-            i += k;
-            j *= 10;
-            j += l;
-        }
-    }
-    putchar('\n');
-    return 0;
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
 }
