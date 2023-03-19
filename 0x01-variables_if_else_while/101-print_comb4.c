2cd75aa86fdf518f112a62abb159f796efc3a0dc
BKaoukine <b.koukin@gmail.com>
2023-03-19 04:19:39 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+/* Online C Compiler and Editor */
+#include <stdio.h>
+/**
+  * main - Strating Point
+  *
+  * Discriptions: this program prints combination of three digits
+  *
+  *Return: 0 for Successe
+  */
+int main(void)
+{
+	int i, a, b;
+
+	for (i = 0 ; i <= 7 ; i++)
+	{
+	for (a = i ; a <= 8 ; a++)
+	{
+		for (b = a ; b <= 9 ; b++)
+		{
+			if (a != b && a != i && i != b)
+			{
+				putchar('0' + i);
+				putchar('0' + a);
+				putchar('0' + b);
+			if (i != 7 || a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			{
+			}
+			}
+			}
+			}
+			}
+			}
+	return (0);
+}
