6ac03c37b94c40ba73c0aec1f36d8aaa916b6811
BKaoukine <b.koukin@gmail.com>
2023-03-19 05:44:46 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,29 +9,31 @@
   */
 int main(void)
 {
-    int i, b, c, e;
+    int i, j, k, l;
 
-    for (i = 0 ; i <= 10 ; i++) {
-        for (b = 0 ; b <= 10; b++) {
-            for (c = b ; c <= 10 ; c++) {
-                for (e = c ; e <= 10; e++) {
-                    if (i != 10 && b != 10 && c != 10 && e != 10) {
-                        if ( e != b) {
-				putchar(' ');
-                            putchar('0' + i);
-                            putchar('0' + b);
-                            putchar(' ');
-                            putchar('0' + c);
-                            putchar('0' + e);
-                            if (b != 8 || c != 9) {
-                                putchar(',');
-                            }
-                        }
-                    }
+    for (i = 0; i <= 99; i++) {
+        for (j = i; j <= 99; j++) {
+            k = i % 10;
+            l = j % 10;
+            i /= 10;
+            j /= 10;
+            if (i != 10 && j != 10 && i != l && j != k) {
+                putchar('0' + i);
+                putchar('0' + k);
+                putchar(' ');
+                putchar('0' + j);
+                putchar('0' + l);
+                if (i != 9 || j != 9) {
+                    putchar(',');
+                    putchar(' ');
                 }
             }
+            i *= 10;
+            i += k;
+            j *= 10;
+            j += l;
         }
     }
     putchar('\n');
-    return (0);
+    return 0;
 }
