703c86e647fe247c7d8493e0e49fc4146a5cc74b
BKaokine <b.koukin@gmail.com>
2023-03-21 20:36:29 +0000

    isalphav1

---
+++ 4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - function checks for lowercase alphabets
+ * @c: c provided as a single alphabet
+ * Discription: print 1 if the alphab is lowercase to the STO
+ *
+ * Return: 0 if Succes
+*/
+int _isalpha(int c)
+{
+	int result;
+
+if (c > 56 && c < 123)
+{
+	result = 1;
+} else
+	{
+		result = 0;
+	}
+	return (result);
+}
+
+
