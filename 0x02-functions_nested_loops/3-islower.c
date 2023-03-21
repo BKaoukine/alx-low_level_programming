b9d9125ce33db2f2947d9c0c57cdb2348ef05ec0
BKaokine <b.koukin@gmail.com>
2023-03-21 17:36:10 +0000

    check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - function prints alphabets to STO
+ *
+ * Discription: print lowercase alphabets to the STO
+ *
+ * Return: 0 if Succes
+*/
+int _islower(int c){
+	if(islower(c) > 0){
+		retrurn(1);
+	}else{
+		return(0);
+	}
+}
