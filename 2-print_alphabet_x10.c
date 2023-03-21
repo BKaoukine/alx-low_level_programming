b36dbfdc1c084133ba8d9e20f8ecddb93915865d
BKaokine <b.koukin@gmail.com>
2023-03-21 12:49:00 +0000

    10x

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - function prints alphabets to STO
+ *
+ * Discription: print lowercase alphabets to the STO
+ *
+ * Return: 0 if Succes
+*/
+/* Online C Compiler and Editor */
+int main()
+{
+    char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, a=0;
+		while(a < 10){
+			for (i = 0; i < 26; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	    if(a < 9)
+	    {
+	_putchar('\n');
+	    }
+	a++;
+		}
+}
