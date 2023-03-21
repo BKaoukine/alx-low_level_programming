c95b379eb14796adf9745c5c4a77b5c58c9c175d
BKaokine <b.koukin@gmail.com>
2023-03-21 21:23:54 +0000

    lastdigi

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - function outputs the absolute value of giving number
+ * @n: n is provided as a number
+ * Discription: reproduces the absolut value of number
+ *
+ * Return: 0 if Succes
+*/
+int print_last_digit(int n)
+{
+	int result;
+
+	result = n % 10 ;
+
+	return (result);
+}
+
+
