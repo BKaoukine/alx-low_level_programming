abccd6d6077a60e5427500e347f2457efbadd8ef
BKaokine <b.koukin@gmail.com>
2023-03-21 12:16:44 +0000

    v8

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - function prints alphabets to STO
+ *
+ * Discription: print lowercase alphabets to the STO
+ *
+ * Return: 0 if Succes
+*/
+
+
+void print_alphabet(void)
+{
+	char alpha[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar('\n');
+
+}
