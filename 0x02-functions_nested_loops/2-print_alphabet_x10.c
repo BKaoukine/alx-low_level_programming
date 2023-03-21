9a851de39008eb7c5c6eda6bc65ab24b5ddba0e5
BKaokine <b.koukin@gmail.com>
2023-03-21 16:14:07 +0000

    betty enhancement

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet - function prints alphabets to STO
+ * print_alphabet_x10 - function prints alphabets to STO
  *
  * Discription: print lowercase alphabets to the STO
  *
  * Return: 0 if Succes
 */
-void print_alphabet_x10(void){
-    char alpha[] = "abcdefghijklmnopqrstuvwxyz";
-	int i, a=0;
-		while(a < 10){
-			for (i = 0; i < 26; i++)
+void print_alphabet_x10(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i, a = 0;
+
+	while (a < 10)
+	{
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(alpha[i]);
 	}
-	    if(a < 10)
-	    {
+	if (a < 10)
+	{
 	_putchar('\n');
-	    }
+	}
 	a++;
 		}
 }
