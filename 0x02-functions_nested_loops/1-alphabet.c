0c88e79994bec0f33363cfd6cb6e9a3951d0da6f
BKaoukine <b.koukin@gmail.com>
2023-03-21 02:01:04 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+
+int main (void)
+{
+
+	char alpha[]="abcdefghijklmnopqrstuvwxyz";
+
+	for(int i = 0; i < 25;i++)
+	{
+		_putchar(alpha[i];
+	}
+
+	return(0);
+
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory holds C programms in which Functions and loops are manupilated
