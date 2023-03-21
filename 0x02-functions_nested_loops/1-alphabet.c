0d1df4faa7e887e787d5c11810295854db7124a1
BrahimSE <b.koukin@gmail.com>
2023-03-21 11:34:10 +0000

    v8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,19 @@
 #include "main.h"
 /**
  * print_alphabet - function prints alphabets to STO
- * 
+ *
  * Discription: print lowercase alphabets to the STO
- * 
+ *
  * Return: 0 if Succes
 */
 
 
 void print_alphabet(void)
 {
-	char alpha[]="abcdefghijklmnopqrstuvwxyz";
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for(i = 0; i < 26;i++)
+
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(alpha[i]);
 	}
