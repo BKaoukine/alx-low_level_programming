49705df8bb265c5c45287bd77e2f58d735693c7c
BKaokine <b.koukin@gmail.com>
2023-03-22 00:34:15 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - function outputs the absolute value of giving number
+ * Discription: reproduces the absolut value of number
+ *
+ * Return: 0 if Succes
+*/
+int add(int i, int j)
+{
+	int i, j, sum;
+
+	sum = i +j;
+
+	_putchar('0'+sum);
+	return(0);
+
+}
+
+
