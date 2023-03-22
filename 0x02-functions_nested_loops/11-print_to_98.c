a52569b5333105d78745a3edb44b9d2f9c779632
BKaokine <b.koukin@gmail.com>
2023-03-22 12:18:17 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,40 +8,43 @@
  *
  * Return: 0 if Succes
 */
-void print_to_98(int n)
-{
-/*
-Numbers must be separated by a comma, followed by a space
-Numbers should be printed in order
-The first printed number should be the number passed to your function
-The last printed number should be 98
-You are allowed to use the standard library
-*/
-		if(n > 0 && n < 98)
-		{
-			while (n != 98)
-			{
-				_putchar('0'+n);
-				n++;
-			}
-				
-		}else if(n > 98)
-		{
-			while(n>98)
-			{
-				_putchar('0'+n);
-				n--;
-			}
-		}else
-		{
-			while (n != 98)
-			{
-				_putchar('0'+ n);
-				n++;
-			}
-			
-		}
+void print_to_98(int n) {
+        /*
+        Numbers must be separated by a comma, followed by a space
+        Numbers should be printed in order
+        The first printed number should be the number passed to your function
+        The last printed number should be 98
+        You are allowed to use the standard library
+        */
+        if(n > 0 && n < 98) {
+            while (n != 99) {
+                printf("%i",n);
+                if (n < 98){
+                     printf(", ");
+                }
+                n++;
+            }
 
-}
+        } else if(n > 98) {
+            while(n>97) {
+                printf("%i",n);
+                if (n > 98){
+                     printf(", ");
+                }
+                n--;
+            }
+        } else {
+            while (n != 99) {
+                printf("%i",n);
+                if (n < 98){
+                     printf(", ");
+                }
+                n++;
+            }
+
+        }
+
+    }
+	
 
 
