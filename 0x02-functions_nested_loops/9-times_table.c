55f365f5a60d294585834cad45996417cdd793cb
BKaokine <b.koukin@gmail.com>
2023-03-22 11:27:25 +0000

    learntable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,35 @@
 #include "main.h"
-
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * times_table- prints the 9 times table start with 0
+ *
+ * Return: void
  */
-void print_times_table(int n)
+void times_table(void)
 {
-	int num, mult, prod;
+	int n, m, mult;
 
-	if (n >= 0 && n <= 15)
+	for (n = 0; n <= 9; n++)
 	{
-		for (num = 0; num <= n; num++)
+		for (m = 0; m <= 9; m++)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
+			mult = (m * n);
+			if (mult <= 9)
+			{
+				_putchar(mult + 48);
+			}
+			else
+			{
+				_putchar(mult / 10 + 48);
+				_putchar(mult % 10 + 48);
+			}
+			if (m < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
+			if ((n * (m + 1)) <= 9 &&  m != 9)
+				_putchar(' ');
 		}
+		_putchar('\n');
 	}
 }
