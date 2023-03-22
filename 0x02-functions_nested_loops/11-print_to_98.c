2ffdfa5a35adc288a62a4ffbe3c5d92f9cc0ca9b
BKaokine <b.koukin@gmail.com>
2023-03-22 16:43:17 +0000

    final test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,28 @@
 */
 void print_to_98(int n)
 {
-	while (n != 98)
+while (n < 98)
 {
-		printf("%i", n);
-	if (n != 98)
-		printf(", ");
-	if (n < 0)
-		n++;
-	else if (n >= 0 && n <= 98)
-		n++;
-else if(n > 98)
+printf("%i", n);
+if (n != 98)
 {
-		n--;
+printf(", ");
 }
+n++;
+}
+while (n > 98)
+{
+printf("%i", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+if (n == 98)
+{
+printf("%i", n);
 }
 printf("\n");
 }
+
