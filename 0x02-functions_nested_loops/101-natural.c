11d1b443cbd38e88ce60deb487da26191525e19f
BKaokine <b.koukin@gmail.com>
2023-03-22 22:36:48 +0000

    all multiple

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function outputs the sum of 3 and 5 multiple
+ *
+ * Discription: sum of multiples of 3 and 5
+ *
+ * Return: 0 if Succes
+*/
+int main(void)
+{
+int sum = 0;
+
+for (int i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
