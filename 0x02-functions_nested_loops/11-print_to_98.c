5e88b8df667580f1c3823b0bdf94a10ebdb9bbcb
BKaokine <b.koukin@gmail.com>
2023-03-22 12:51:54 +0000

    aay

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,60 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * add - function outputs the absolute value of giving number
- * @i: numbers given
- * @j:numbers given
+ * print_to_98 - function outputs the absolute value of giving number
+ * @n: numbers given
  * Discription: reproduces the absolut value of number
  *
  * Return: 0 if Succes
 */
-void print_to_98(int n) {
-        /*
-        Numbers must be separated by a comma, followed by a space
-        Numbers should be printed in order
-        The first printed number should be the number passed to your function
-        The last printed number should be 98
-        You are allowed to use the standard library
-        */
-        if(n > 0 && n < 98) {
-            while (n != 99) {
-                printf("%i",n);
-                if (n < 98){
-                     printf(", ");
-                }else if (n == 98)
-				{
-					printf("\n");
-				}
-				
-                n++;
-            }
-
-        } else if(n > 98) {
-            while(n>97) {
-                printf("%i",n);
-                if (n > 98){
-                     printf(", ");
-                }else if (n == 98)
-				{
-					printf("\n");
-				}
-                n--;
-            }
-        } else {
-            while (n != 99) {
-                printf("%i",n);
-                if (n < 98){
-                     printf(", ");
-                }else if (n == 98)
-				{
-					printf("\n");
-				}
-                n++;
-            }
-
-        }
-
-    }
-
-
-
+void print_to_98(int n)
+{
+	while (n != 99)
+{
+		printf("%i", n);
+	if (n != 98)
+		printf(", ");
+	if (n < 0)
+		n++;
+	else if (n > 0 && n < 99)
+		n++;
+else
+{
+		n--;
+}
+}
+printf("\n");
+}
