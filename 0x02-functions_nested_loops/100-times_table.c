8fd52d6043a219c8c81862e1a7fe9593d3a1fdd5
BKaokine <b.koukin@gmail.com>
2023-03-22 19:53:32 +0000

    qs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,8 +14,7 @@ void print_times_table(int n) {
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++)
 		{
-            printf("%-4d", i * j);
-			printf(",");
+            printf("%-4d,", i * j);
         }
         printf("\n");
     }
