f5a16eb416c29922bb76f4d667129a5f92f58422
BKaokine <b.koukin@gmail.com>
2023-03-22 00:26:38 +0000

    blah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
 */
 
 int main(void){
-
-	char alpha[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(alpha[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return(O);
+	return (0);
 }
