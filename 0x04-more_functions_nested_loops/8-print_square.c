2cecb102a31bc78de59c112660cf7f2277c40062
BKaokine <b.koukin@gmail.com>
2023-03-23 21:39:26 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - function print lines using dash
+ *@n: number of dashes to use to print line
+ * Discription: print line
+ *
+ * Return: 0 if Succes
+*/
+void print_square(int size)
+{
+	int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+	_putchar(35);
+}
+}
+_putchar('\n');
+}
