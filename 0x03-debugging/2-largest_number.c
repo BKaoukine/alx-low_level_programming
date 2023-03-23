09323511eb2bcb498ccd70e15a963d4a43438328
BKaokine <b.koukin@gmail.com>
2023-03-23 05:22:24 +0000

    deb

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,15 +11,12 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-
-largest = a;
-if (b > largest)
-{
-largest = b;
-}
-if (c > largest)
-{
-largest = c;
-}
+    if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a && b >= c) {
+        largest = b;
+    } else {
+        largest = c;
+    }
 return (largest);
 }
