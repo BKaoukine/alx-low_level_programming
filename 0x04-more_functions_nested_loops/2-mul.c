827d66a0754d55d8265c8c505399ec8c49cab8b3
BKaokine <b.koukin@gmail.com>
2023-03-23 15:16:54 +0000

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - function calculate multiplication
+ * @a: c provided as a  number
+ * @b: b provided as a number
+ * Discription: calculate multiplication of two numbers
+ *
+ * Return: 0 if Succes
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
