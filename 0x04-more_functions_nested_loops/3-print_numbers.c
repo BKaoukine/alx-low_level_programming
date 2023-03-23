c9b67de7dca46d47c25ec13bff9d322a51a91662
BKaokine <b.koukin@gmail.com>
2023-03-23 15:24:42 +0000

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - function print numbers from 0 to 9
+ *
+ * Discription: print numbers
+ *
+ * Return: 0 if Succes
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	return (0);
+}
