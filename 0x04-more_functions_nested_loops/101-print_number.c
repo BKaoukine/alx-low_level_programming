2023690d5330a9664336e3d73ad811e29d73f40b
BKaokine <b.koukin@gmail.com>
2023-03-24 11:02:46 +0000

    nulbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,17 +8,15 @@
 */
 void print_number(int n)
 {
-if (n < 0)
-{
-putchar('-');
-n = -n;
-}
+	unsigned int i = n;
 
-if (n / 10)
-{
-print_number(n / 10);
-}
-putchar((n % 10) + '0');
-
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if ((i / 10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
 
