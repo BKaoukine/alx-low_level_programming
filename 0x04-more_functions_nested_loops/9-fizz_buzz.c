c3a9113026edb14fb8c5d8f4fdebc55bfe2543e5
BKaokine <b.koukin@gmail.com>
2023-03-24 02:09:33 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program for the FizzBuzz test
+ * Discription: print Fizz or Buzz or both depanding on mutliplier
+ *
+ * Return: 0 if Succes
+*/
+int main(void)
+{
+int i, isMulti3, isMulti5;
+
+for (i = 1 ; i <= 100; i++)
+{
+isMulti3 = i % 3;
+isMulti5 = i % 5;
+if (isMulti3 == 0 && isMulti5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (isMulti3 == 0)
+{
+printf("Fizz ");
+}
+else if (isMulti5 == 0)
+{
+printf("Buzz ");
+}
+else
+printf("%i", i);
+}
+}
