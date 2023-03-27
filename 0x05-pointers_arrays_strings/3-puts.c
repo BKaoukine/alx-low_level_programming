6a6d2b2e9827c2724eb61df5f1219ab83bb1be64
BKaokine <b.koukin@gmail.com>
2023-03-27 17:14:48 +0000

    print a string to STO v2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,13 +7,13 @@
  *
  * Return: 0 if Succes
 */
-int _puts(char *str)
+void _puts(char *str)
 {
 int i = 0;
 
 while (*(str + i) != 0)
 {
-putchar(*(str + i));
+_putchar(*(str + i));
 i++;
 }
 return (i);
