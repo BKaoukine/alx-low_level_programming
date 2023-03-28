5a29420fd43eb303ad0409b36dc01723ed061e9a
BKaokine <b.koukin@gmail.com>
2023-03-28 15:35:20 +0000

    strcpy v1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,17 @@
 */
 char *_strcpy(char *dest, char *src)
 {
+	char *original_dest = dest;
 while (*src)
 {
 *dest = *src;
 src++;
 dest++;
 }
-//*dest = '\0';
-	return (0);
+*dest = '\0';
+<<<<<<< HEAD
+	return (original_dest);
+=======
+  return (0);
+>>>>>>> e5af9a08cf9713313d3ce8ec27bba94b98e546aa
 }
