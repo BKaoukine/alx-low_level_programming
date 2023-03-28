b5fdb52298cf07e971e2fc32db10e39845737ecc
BKaokine <b.koukin@gmail.com>
2023-03-28 10:51:27 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - function copies string
+ *@dest: string destination
+ *@src: source of the string
+ * Discription: copies a string from src to dest
+ * Return: 0 if Succes
+*/
+char *_strcpy(char *dest, char *src)
+{
+while (*src)
+{
+*dest = *src;
+src++;
+dest++;
+}
+*dest = '\0';
+}
+
