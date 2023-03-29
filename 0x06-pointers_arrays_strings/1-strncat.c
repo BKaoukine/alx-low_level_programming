ea8e5675bbf94c2a1821fe584de0e3ba0c2b0362
BKaokine <b.koukin@gmail.com>
2023-03-29 23:26:44 +0000

    bla strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,45 +1,34 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _strncat - function concatenate two string
- *@dest: string destination
- *@src: source of the string
- *@n: given number of bytes
- * Discription: copies a string from src to dest and concatenat both
- * Return: 0 if Succes
-*/
+ * _strncat - concatenates two strings
+ * @dest: first parameter - string that the concatenated string will be saved into
+ * @src: second parameter - string that will be concatenated to dest
+ * @n: third parameter - maximum number of bytes from src that can be concatenated to dest
+ *
+ * Description: This function concatenates two strings, but will only use at most n bytes from src.
+ * If src contains n or more bytes, then the string will not be null-terminated.
+ *
+ * Return: pointer to string dest
+ */
+
 char *_strncat(char *dest, char *src, int n)
 {
-/* use a working pointer to preserve dest for the return value */
-char *d = dest;
-int lenofsrc = _strlen(src);
-int i;
+    int i, concat; // declare variables i and concat
 
-/* find the offset of the null terminator in dest */
-while (*d != '\0')
-{
-d++;
-}
-/* check if n is less or grater than lenght of src*/
-if (n < lenofsrc)
-{
-/* copy the first "n" bytes from the src string there */
-*d++ = *src++;
-}
-/* check if n is greater or equal than lenght of src*/
-else if (n >= lenofsrc)
-{
-for (i = 0; i < lenofsrc; i++)
-{
-/* copy all bytes from the src string there */
-*d++ = *src++;
-}
-}
-/* set the null terminator */
-if (n > lenofsrc)
-{
-*d++ = '\0';
-}
-/* return the pointer to the destination array */
-return (dest);
+    // loop through the dest string until null terminator is found
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+    }
+
+    // concatenate characters from src to dest until n bytes have been appended
+    for (concat = 0; concat < n; concat++)
+    {
+        dest[i + concat] = src[concat]; // concatenate character from src to dest
+        if (src[concat] == '\0') // if end of src string is reached
+            concat = n; // exit the loop
+    }
+
+    return (dest); // return pointer to string dest
 }
