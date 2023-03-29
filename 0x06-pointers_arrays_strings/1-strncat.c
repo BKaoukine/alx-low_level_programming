15cb5eb54822d600d83c4a141908c47d8477f089
BKaokine <b.koukin@gmail.com>
2023-03-29 23:19:01 +0000

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 {
 /* use a working pointer to preserve dest for the return value */
 char *d = dest;
-int lenofsrc = strlen(src);
+int lenofsrc = _strlen(src);
+int i;
 
 /* find the offset of the null terminator in dest */
 while (*d != '\0')
@@ -26,11 +27,13 @@ if (n < lenofsrc)
 *d++ = *src++;
 }
 /* check if n is greater or equal than lenght of src*/
-while (n >= lenofsrc)
+else if (n >= lenofsrc)
+{
+for (i = 0; i < lenofsrc; i++)
 {
 /* copy all bytes from the src string there */
 *d++ = *src++;
-n--;
+}
 }
 /* set the null terminator */
 *d = '\0';
