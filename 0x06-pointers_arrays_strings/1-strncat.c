5788d8eaa6349e0ded9ac8ba9b4656bd8e5ab260
BKaokine <b.koukin@gmail.com>
2023-03-29 23:28:02 +0000

    bla strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,32 +3,26 @@
 
 /**
  * _strncat - concatenates two strings
- * @dest: first parameter - string that the concatenated string will be saved into
- * @src: second parameter - string that will be concatenated to dest
- * @n: third parameter - maximum number of bytes from src that can be concatenated to dest
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: bytes fron src to be appended to dest
  *
- * Description: This function concatenates two strings, but will only use at most n bytes from src.
- * If src contains n or more bytes, then the string will not be null-terminated.
- *
- * Return: pointer to string dest
+ * Return: string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, concat; // declare variables i and concat
+	int i, concat;
 
-    // loop through the dest string until null terminator is found
-    for (i = 0; dest[i] != '\0'; i++)
-    {
-    }
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
 
-    // concatenate characters from src to dest until n bytes have been appended
-    for (concat = 0; concat < n; concat++)
-    {
-        dest[i + concat] = src[concat]; // concatenate character from src to dest
-        if (src[concat] == '\0') // if end of src string is reached
-            concat = n; // exit the loop
-    }
-
-    return (dest); // return pointer to string dest
+	for (concat = 0; concat < n; concat++)
+	{
+		dest[i + concat] = src[concat];
+		if (src[concat] == '\0')
+			concat = n;
+	}
+	return (dest);
 }
