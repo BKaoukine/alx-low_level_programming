174d9bdf96562bed3f6f141d852c16cf256fe145
BKaokine <b.koukin@gmail.com>
2023-03-30 23:28:53 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * leet - encodes a strings
+ * @str: the string to encode
+ *
+ * Return: the modified string
+ */
+
+	char *leet(char *str)
+{
+char *letters = "aAeEoOtTlL";
+char *leet = "4433007711";
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = leet[j];
+break;
+}
+}
+}
+return (str);
+}
+}
