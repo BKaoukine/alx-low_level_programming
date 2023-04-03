c5f28379145d48340baca978af386abe5ad90b83
BKaokine <b.koukin@gmail.com>
2023-04-03 15:22:22 +0000

    Add _strchr function implementation

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strchr - locate a character in a string
+*@s: string to search
+*@c: character to find
+*Return: c or NULL if the character is not found.
+*/
+char *_strchr(char *s, char c)
+{
+		while (*s)
+	{
+			if (*s == c)
+			return (s);
+			if (*s == NULL)
+			return (NULL);
+		s++;
+	}
+	return (s);
+}
