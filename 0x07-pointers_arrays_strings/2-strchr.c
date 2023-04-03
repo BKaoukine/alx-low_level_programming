81d859137fb1b31e8dc85226788667acd08102db
BKaokine <b.koukin@gmail.com>
2023-04-03 15:48:14 +0000

    Add _strchr function implementation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,19 +3,19 @@
 * _strchr - locate a character in a string
 * @s: string to search
 * @c: character to find
-*
 * Description: locate 1st occurrence Char c in the string s or NULL if none
+*
 * Return: c or NULL if the character is not found.
 */
 char *_strchr(char *s, char c)
 {
-		while (*s)
-	{
-			if (*s == c)
-			return (s);
-			if (*s == '\0')
-			return (NULL);
-		s++;
-	}
-	return (s);
+while (*s)
+{
+if (*s == c)
+return (s);
+if (*s == '\0')
+return (NULL);
+s++;
+}
+return (s);
 }
