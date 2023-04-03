07e5ccb42ac483ef6c1c3994fbb4cfdbc504bcca
BKaokine <b.koukin@gmail.com>
2023-04-03 15:52:42 +0000

    Add _strchr function implementation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s)
-    {
-        if (*s == c)
-            return (s);
-        if (*s == '\0')
-            return (NULL);
-        s++;
-    }
-    return (s);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return (s);
 }
