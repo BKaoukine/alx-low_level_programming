cb8fe788a207d6294701b9f1cdcdeb41525f6402
BKaokine <b.koukin@gmail.com>
2023-04-05 18:47:03 +0000

    Implement _puts (reverse) function using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion - function Prints a string
+ * @s: string to print
+ *
+ * Description: print the given string using recursive Call
+ *
+ * Return: a string if succes
+ */
+void _print_rev_recursion(char *s)
+{
+
+/*Check if the String is not NULL*/
+		if (*s)
+		{
+/*Call the function recursivly to move to the other Characaters*/
+			_puts_recursion(s + 1);
+/*Print the last Character*/
+			_putchar(*s);
+		} else
+		_putchar('\n');
+
+}
