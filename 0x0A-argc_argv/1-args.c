de5eea6339cac2ce0d27e2fad77d7d56de8be8f8
BKaokine <b.koukin@gmail.com>
2023-04-08 17:55:28 +0000

    program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * @argc: The lenght of the compiling parameters
+ * @argv: The actual compiling parameters
+ *
+ *Description: function that prints lenght of parameters used
+ * Return: On success 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
