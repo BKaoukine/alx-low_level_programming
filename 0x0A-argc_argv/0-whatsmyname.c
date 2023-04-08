f2037245e7ff2bb7af5d6fe03761032d0782e541
BKaokine <b.koukin@gmail.com>
2023-04-08 17:50:05 +0000

    program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Starting point
+ * @argc: The lenght of the compiling parameters
+ * @argv: The actual compiling parameters
+ *
+ *Description: function that prints it's name
+ * Return: On success 1.
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-  return 0;
+	return (0);
 }
