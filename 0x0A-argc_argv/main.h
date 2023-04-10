d5bc2cda7304efc44982c081271c5c40f5eeae9f
BKaokine <b.koukin@gmail.com>
2023-04-10 08:18:56 +0000

    program that adds  given numbers.

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
+#include <ctype.h>
+ #include <stdbool.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
 int _putchar(char c);
 #endif /* MAIN_H */
