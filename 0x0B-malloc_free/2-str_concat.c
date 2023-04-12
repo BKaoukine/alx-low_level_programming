06eb2e3e16373c9d730407f948f9e26e0dc213da
BKaokine <b.koukin@gmail.com>
2023-04-12 01:56:50 +0000

    a function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * str_concat - function copies string
+ *@s1: char to copy
+ *@s2: char to copy
+ * Discription: create a char with specified size of memory
+ * Return: 0 if Succes
+*/
+char *str_concat(char *s1, char *s2)
+{
+char *strcopy;
+unsigned int i = strlen(s1) + strlen(s2);
+if (s1 == NULL || s2 == NULL)
+{
+return (NULL);
+}
+else
+{
+strcopy = (char *) malloc(i * sizeof(char));
+if (strcopy == NULL)
+return (NULL);
+
+while (s1 != '\0')
+{
+	strcopy = s1;
+	strcopy++;
+}
+while (s2 != '\0')
+{
+	strcopy = s2;
+	strcopy++;
+}
+
+}
+strcopy[i] = '\0';
+}
+return (strcopy);
+}
+
+
