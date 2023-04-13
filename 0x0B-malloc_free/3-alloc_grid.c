1e3a2d09220d14dedb50ff8603c07f3778012a6e
BKaokine <b.koukin@gmail.com>
2023-04-13 02:06:45 +0000

    function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,27 +3,44 @@
  * alloc_grid - create a grid of numbers
  * @width: width of the grid
  * @height: height of the grid
- * Return: pointer to concatenated string
+ * Return: pointer to 2D array
  */
 int **alloc_grid(int width, int height)
 {
 	int i, j;
-	int *grid = malloc(width * height * sizeof(int));
-	int *p[2];
+	int **grid; /* Declare a pointer to a 2D array of integers */
 
-	if (width == 0 || height == 0)
-	return (NULL);
-	if (width < 0 || height < 0)
-	return (NULL);
+	if (width <= 0 || height <= 0) /* Check for invalid input */
+		return (NULL);
 
-	for (i = 0; i < width; i++)
+	/* Allocate memory for the 2D array of integers */
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)/* Check for allocation failure */
+		return (NULL);
+
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		/* Allocate memory for each row of the 2D array */
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)/* Check for allocation failure */
 		{
-			*grid[i][j] = {0};
+			/* Free previously allocated memory and return NULL */
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
 		}
-
 	}
 
-	return (*grid);
+	/* Initialize each element of the 2D array to zero */
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+/*Return a pointer to the 2D array*/
+return (grid);
 }
