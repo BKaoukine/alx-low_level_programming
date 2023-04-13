7e1bc39cca73ffafd9aa879fa7d42d2c0c8a4318
BKaokine <b.koukin@gmail.com>
2023-04-13 15:30:11 +0000

    function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * malloc_checked - function copies string
+ *@b: size of the memory to allocat for the array
+ * Discription: allocate memory for given size
+ * Return: 0 if Succes
+*/
+void *malloc_checked(unsigned int b);
+{
+	int *allocmem;
+
+	allocmem = malloc(b * sizeof(int);)
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This folder contains more different project on Dynamic Memory Allocation in C using malloc(), calloc(), free() and realloc()
