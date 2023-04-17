5508b666e4afafe702d9332849cd412b5b749b77
BKaokine <b.koukin@gmail.com>
2023-04-17 16:31:02 +0000

    define new type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+* struct dog - struct dog type
+*@name: dog's name
+*@age: age of the dog
+*@owner: name of the dog's owner
+*/
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 
 
 
