0186183ddc2fbdd0467cbea29b5e729178e24e8e
BKaokine <b.koukin@gmail.com>
2023-04-18 00:00:30 +0000

    function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,18 +16,13 @@ if (d == NULL)
 return;
 }
 
-if (d->name[0] == '\0')
+if (d->name == NULL)
 printf("Name: (nil)\n");
-else
-printf("Name: %s\n", d->name);
 
-if (d->age <= 0)
-printf("Age: (nil)\n");
-else
-printf("Age: %f\n", d->age);
-
-if (d->owner[0] == '\0')
+if (d->owner == NULL)
 printf("Owner: (nil)\n");
-else
+
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
 }
