dae433903eada13d895c40e549dae06016cb84a1
BKaokine <b.koukin@gmail.com>
2023-04-19 15:58:33 +0000

    a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - starting point
+ * @name: pointer to name to print
+ * @f: pointer to function with char arguement
+ * Description: function that prints a name using pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	 f(name);
+}
