bf4ec3ee456541e3e7f03656b3a8ed30d53c7d5b
BKaokine <b.koukin@gmail.com>
2023-04-21 01:00:52 +0000

    function that returns the sum of all its parameters.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,9 +3,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-/* #include <stddef.h>
-#include <string.h>
-#include <stdlib.h> */
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
