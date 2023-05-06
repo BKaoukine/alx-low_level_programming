aecb5d747a38cd80becfb6afb11ebd984e83e3a9
BKaoukine <b.koukin@gmail.com>
2023-05-06 01:53:51 +0100

    reforme the function using recursive approach

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,18 +5,8 @@
  */
 void print_binary(unsigned long int n)
 {
-int i;
+    if (n > 1)
+        print_binary(n >> 1);
 
-for (i = 63; i >= 0; i--)
-{
-if ((n >> i) & 1)
-{
-_putchar('1');
+    _putchar((n & 1) + '0');
 }
-else
-{
-_putchar('0');
-}
-}
-}
-
