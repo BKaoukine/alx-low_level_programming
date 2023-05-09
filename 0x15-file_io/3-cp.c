ed502c368b23ad69ea8e4072ce9bfaac2e63947d
BKaoukine <b.koukin@gmail.com>
2023-05-09 23:53:53 +0100

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,28 +7,22 @@
  */
 void cpfile(int argc, char *argv[])
 {
-char *error, mybuffer[MY_BUFFER_SIZE];
+char mybuffer[MY_BUFFER_SIZE];
 int fileopen, fileopento, bytes_read, bytes_written;
 
 if (argc != 3)
-{
 printf("Usage: cp file_from file_to\n");
 exit(97);
-}
 
 fileopen = open(argv[1], O_RDONLY);
 if (fileopen == -1)
-{
 printf("Error: Can't read from file %s\n", argv[1]);
 exit(98);
-}
 
 fileopento = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 if (fileopento == -1)
-{
 printf("Error: Can't write to %s\n", argv[2]);
 exit(99);
-}
 
 while ((bytes_read = read(fileopen, mybuffer, MY_BUFFER_SIZE)) > 0)
 {
@@ -37,24 +31,19 @@ if (bytes_written == -1)
 {
 printf("Error: Can't write to %s\n", argv[2]);
 exit(99);
-}
-}
-
+}}
 if (bytes_read == -1)
 {
 printf("Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
 if (close(fileopen) == -1)
 {
 printf("Error: Can't close fd %d\n", fileopen);
 exit(100);
 }
-
 if (close(fileopento) == -1)
 {
 printf("Error: Can't close fd %d\n", fileopento);
 exit(100);
-}
-}
+}}
