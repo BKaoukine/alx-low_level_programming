297fba3b91eade0d35f99dbb5d122d08cda29bb4
BKaoukine <b.koukin@gmail.com>
2023-05-09 23:54:05 +0100

    3. cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,8 +14,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void cpfile(const char "filename1", const char "filename2")
-;
+void cpfile(int argc, char *argv[]);
 
 
 
