20475636acf602cf927083819aed67851a839548
BKaoukine <b.koukin@gmail.com>
2023-09-04 19:27:51 +0100

    reading a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <main.h>
+
+/**
+ * read_textfile - Strating Point
+ * @filename: the file to read
+ * @letters: number of bytes to read
+ *
+ * Description: reads a text file and prints it to the POSIX standard output.
+ * Return: number of bytes printed on Succes or 0 on failure
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fopen, fread, fwrite;
+	char *fileBuff;
+
+	if (filename == NULL)
+		return (0);
+
+	fopen = open(filename, O_RDONLY); /*Opening the File*/
+	if (fo == -1)
+		return (0);
+
+
+	fileBuff = malloc(sizeof(char) * letters);/*Allocating memory for the buffer*/
+	fread = read(fopen, fileBuff, letters);/*Reading from the file*/
+	fwrite = write(STDOUT_FILENO, fileBuff, fread);/*Writing required size into STDOUT*/
+
+
+	free(fileBuff);
+	close(fopen);
+	return (fwrite);
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Included Libraries*/
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*Functions Prototypes*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif /*End of MAIN */
