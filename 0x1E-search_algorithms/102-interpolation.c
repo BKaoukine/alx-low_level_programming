2e3f7994de9eeb54d0f5846c0ab70daf5c1c0114
bkaoukine <b.koukin@gmail.com>
2024-03-16 10:13:20 +0000

    interpolation_search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,62 +1,39 @@
 #include "search_algos.h"
 /**
- * array_length - Computes the length of an integer array.
- * @array: Pointer to the first element of the array.
- *
- * Return: Length of the array.
+ *interpolation_search - function that  searches
+ *for a value in a sorted array of integers
+ *@array: pointer to the first element of the array to search in
+ *@size: number of elements in array
+ *@value: the value to search for
+ *Return: first index where value is located
  */
-size_t array_length(int *array)
-{
-size_t length = 0;
-
-while (array[length] != 0)
-length++;
-
-
-return (length);
-}
-/**
- *interpolation_search - a function that searches for a value in a sorted
-*array of integers using the interpolation search algorithm
-*@array: is a pointer to the first element of the array to search in
-*@size: is the number of elements in array
-*@value: is the value of the search
-*Return: index of value or -1
-*/
 int interpolation_search(int *array, size_t size, int value)
 {
-size_t pos, low, high, length;
+	size_t i, l, r;
 
-if (array == NULL || size == 0)
-return (-1);
+	if (array == NULL)
+		return (-1);
 
-low = 0;
-high = size - 1;
-length = array_length(array);
+	for (l = 0, (r = size - 1); r >= l;)
+	{
+		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		if (i < size)
+		{
+			printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+		}
+		else
+		{
+			printf("Value checked array [%ld] is out of range\n", i);
+			break;
+		}
 
-while (low <= high && value >= array[low] && value <= array[high])
-{
-pos = low + ((double)(high - low) / (array[high] - array[low])) *
-(value - array[low]);
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
 
-if (size > length - 1)
-{
-printf("Value checked array[%lu] is out of range\n", pos);
-return (-1);
-}
-
-printf("Value checked array[%lu] = [%i]\n", pos, array[pos]);
-
-if (array[pos] == value)
-return (pos);
-
-else if (array[pos] > value)
-high = pos - 1;
-
-else if (array[pos] < value)
-low = pos + 1;
-
-}
-
-return (-1);
 }
